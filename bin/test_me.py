728dca6b3f801acd1e6592cb5b13214c189da870
Martin J. Hsu <martin.hsu@gmail.com>
2019-07-15 23:30:55 +0800

    changed some asserts to use status_code, added test to delete initial users

--- bin/test_me.py
+++ bin/test_me.py
@@ -20,10 +20,11 @@ initial_users = [
 ]
 
 def test_load_users():
+    # post each user
     for user in initial_users:
-        requests.post(f'http://localhost:5000/user/{user["name"]}', data = user)
-    r = requests.get('http://localhost:5000/user')
-    assert sorted(user['name'] for user in r.json()) == sorted(user['name'] for user in initial_users)
+        r = requests.post(f'http://localhost:5000/user/{user["name"]}', data = user)
+        assert r.status_code == 201
+
 
 def test_get():
     r = requests.get('http://localhost:5000/user/Jass')
@@ -33,20 +34,26 @@ def test_get():
         "occupation": "Web Developer"
     }
 
+
 def test_insert():
     r = requests.post('http://localhost:5000/user/Howard', data = {'age':20, 'occupation':'climber'})
-    assert r.json() == {
-        "name": "Howard",
-        "age": "20",
-        "occupation": "climber"
-    }
+    assert r.status_code == 201
+
 
 # this will fail because it already exists
 def test_insert_duplicate():
     r = requests.post('http://localhost:5000/user/Howard', data = {'age':20, 'occupation':'climber'})
     assert r.status_code == 400
 
+
 def test_delete():
     r = requests.delete('http://localhost:5000/user/Howard')
     assert r.status_code == 200
+
+
+def test_delete_users():
+    # delete each user
+    for user in initial_users:
+        r = requests.delete(f'http://localhost:5000/user/{user["name"]}')
+        assert r.status_code == 200
 
